d84602b0f15e70e41b2fbe122327373998c5164a
Manollas <manollasm1997@gmail.com>
2026-05-09 13:02:38 +0530

    phase 2 & 3 training scripts

---
+++ slm/train/train_phase2.py
@@ -0,0 +1,125 @@
+import torch
+from datasets import load_dataset
+from transformers import (
+    GPT2Config,
+    GPT2LMHeadModel,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments
+)
+import os
+from datasets import load_dataset, load_from_disk
+from itertools import chain
+from common.utils import *
+from common.data_process import tokenize_opentxt,group_texts_opentxt
+
+# ============================================================
+# PATHS
+# ============================================================
+# BASE_PATH = "C:/RP/hospital_management_api/slm"
+BASE_PATH=os.getenv("BASE_PATH")
+LOG_PATH = f"{BASE_PATH}/logs"
+LOG_PATH_PHASE2=LOG_PATH+"/phase2"
+os.makedirs(f"{LOG_PATH}/phase2", exist_ok=True)
+os.environ["TENSORBOARD_LOGGING_DIR"] = LOG_PATH_PHASE2
+
+# ============================================================
+# CONFIG
+# ============================================================
+BLOCK_SIZE = os.getenv("BLOCK_SIZE")
+PHASE2_RAW_PATH = os.getenv("PHASE2_RAW_PATH")
+PHASE2_PROCESSED_PATH = os.getenv("PHASE2_PROCESSED_PATH")
+
+PHASE2_MODEL_CHECKPOINT_PATH = os.getenv("PHASE2_MODEL_CHECKPOINT_PATH")
+PHASE2_MODEL_SAVE_PATH=os.getenv("PHASE2_MODEL_SAVE_PATH")
+PHASE1_MODEL_SAVE_PATH=os.getenv("PHASE1_MODEL_SAVE_PATH")
+
+# ============================================================
+# TOKENIZER
+# ============================================================
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+
+def load_opentxt_data():
+    dataset = load_dataset(
+        "openwebtext",
+        split="train[:20%]",
+        cache_dir=PHASE2_RAW_PATH
+    )
+
+    tokenized = dataset.map(
+        tokenize_opentxt,
+        batched=True,
+        remove_columns=["text"],
+        num_proc=4
+    )
+
+    lm_dataset = tokenized.map(
+        group_texts_opentxt,
+        batched=True,
+        num_proc=4
+    )
+
+    lm_dataset.save_to_disk(PHASE2_PROCESSED_PATH)
+
+# ============================================================
+# 🔵 PHASE 2
+# ============================================================
+def train_model_phase2():
+    print("🚀 Starting Phase 2 model")
+
+    if os.path.exists(PHASE2_PROCESSED_PATH):
+        print("⚡ Loading processed dataset...")
+        lm_dataset = load_from_disk(PHASE2_PROCESSED_PATH)
+    else:
+        print("🔄 Processing dataset...")
+        load_opentxt_data()
+
+    # ---------------- MODEL ----------------
+    model, resumed = load_model_safely(PHASE2_MODEL_CHECKPOINT_PATH)
+
+    if not resumed:
+        print("🚀 Loading Phase 1 model")
+        model = GPT2LMHeadModel.from_pretrained(PHASE1_MODEL_SAVE_PATH)
+
+    model.gradient_checkpointing_enable()
+    model.config.use_cache = False
+
+    # ---------------- TRAINING ----------------
+    training_args = TrainingArguments(
+        output_dir=PHASE2_MODEL_CHECKPOINT_PATH,
+
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=4,
+
+        max_steps=5000,
+
+        learning_rate=5e-5 if resumed else 1e-4,
+        fp16=not resumed,
+
+        lr_scheduler_type="cosine",
+        warmup_steps=300,
+
+        weight_decay=0.01,
+
+        logging_steps=100,
+
+        save_steps=1000,
+        save_total_limit=3,
+
+        report_to="tensorboard",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=lm_dataset,  # ✅ FIXED
+    )
+
+    trainer.train()
+
+    trainer.save_model(PHASE2_MODEL_SAVE_PATH)
+    tokenizer.save_pretrained(PHASE2_MODEL_SAVE_PATH)
+
+train_model_phase2()
---
+++ slm/train/train_phase3.py
@@ -0,0 +1,164 @@
+import os
+import torch
+
+from datasets import load_dataset, load_from_disk
+from transformers import (
+    GPT2LMHeadModel,
+    AutoTokenizer,
+    Trainer,
+    TrainingArguments,
+)
+from common.utils import *
+from common.data_process import format_instruction,tokenize_phase3
+
+# ============================================================
+# PATHS
+# ============================================================
+
+BASE_PATH=os.getenv("BASE_PATH")
+LOG_PATH = f"{BASE_PATH}/logs"
+LOG_PATH_PHASE3=LOG_PATH+"/phase3"
+
+os.makedirs(f"{LOG_PATH}/phase3", exist_ok=True)
+os.environ["TENSORBOARD_LOGGING_DIR"] = LOG_PATH_PHASE3
+
+# ============================================================
+# CONFIG
+# ============================================================
+BLOCK_SIZE = os.getenv("BLOCK_SIZE")
+PHASE3_RAW_PATH = os.getenv("PHASE3_RAW_PATH")
+PHASE3_PROCESSED_PATH = os.getenv("PHASE3_PROCESSED_PATH")
+PHASE3_MODEL_CHECKPOINT_PATH = os.getenv("PHASE3_MODEL_CHECKPOINT_PATH")
+PHASE3_MODEL_SAVE_PATH=os.getenv("PHASE3_MODEL_SAVE_PATH")
+PHASE2_MODEL_SAVE_PATH=os.getenv("PHASE2_MODEL_SAVE_PATH")
+
+# ============================================================
+# TOKENIZER
+# ============================================================
+
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+tokenizer.pad_token = tokenizer.eos_token
+
+# ============================================================
+# LOAD + PROCESS DATASET
+# ============================================================
+def load_alpaca():
+
+    # already processed
+    if os.path.exists(PHASE3_PROCESSED_PATH):
+        print("✅ Loading processed Alpaca dataset...")
+        return load_from_disk(PHASE3_PROCESSED_PATH)
+
+    print("📥 Downloading + processing Alpaca dataset...")
+
+    dataset = load_dataset(
+        "tatsu-lab/alpaca",
+        cache_dir=PHASE3_RAW_PATH
+    )
+
+    dataset = dataset.map(format_instruction)
+
+    tokenized = dataset.map(
+        tokenize_phase3,
+        batched=False,
+        remove_columns=dataset["train"].column_names,
+    )
+
+    tokenized.save_to_disk(PHASE3_PROCESSED_PATH)
+
+    return tokenized
+
+# ============================================================
+# LOAD DATASET
+# ============================================================
+def train_alpaca():
+    
+    dataset = load_alpaca()
+
+    print("🧠 Loading phase2 model...")
+    model, resumed = load_model_safely(PHASE3_MODEL_CHECKPOINT_PATH)
+
+    if not resumed:
+        model = GPT2LMHeadModel.from_pretrained(PHASE2_MODEL_SAVE_PATH)
+
+    model.gradient_checkpointing_enable()
+    model.config.use_cache = False
+
+    training_args = TrainingArguments(
+        output_dir=PHASE3_MODEL_CHECKPOINT_PATH,
+
+        # batching
+        per_device_train_batch_size=4,
+        gradient_accumulation_steps=4,
+
+        # training length
+        max_steps=8000,
+
+        # learning
+        learning_rate=5e-5,
+        lr_scheduler_type="cosine",
+        warmup_steps=200,
+
+        # regularization
+        weight_decay=0.01,
+
+        # mixed precision
+        fp16=True,
+
+        # logging
+        logging_steps=50,
+        save_steps=1000,
+        save_total_limit=3,
+
+        report_to="tensorboard",
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset["train"],
+    )
+
+    trainer.train()
+
+    trainer.save_model(PHASE3_MODEL_SAVE_PATH)
+    tokenizer.save_pretrained(PHASE3_MODEL_SAVE_PATH)
+
+    print("✅ Phase 3 training complete")
+
+
+train_alpaca()
+
+
+# ============================================================
+# TEST MODEL
+# ============================================================
+
+# if __name__ == "__main__":
+
+#     from transformers import pipeline
+
+#     generator = pipeline(
+#         "text-generation",
+#         model="./final-model",
+#         tokenizer=tokenizer,
+#         device=0 if torch.cuda.is_available() else -1,
+#     )
+
+#     prompt = """### Instruction:
+# Explain why the sky appears blue during the day.
+
+# ### Response:
+# """
+
+#     output = generator(
+#         prompt,
+#         max_new_tokens=200,
+#         do_sample=True,
+#         temperature=0.7,
+#         top_p=0.9,
+#         repetition_penalty=1.2,
+#     )
+
+#     print("\n================ MODEL OUTPUT ================\n")
+#     print(output[0]["generated_text"])
